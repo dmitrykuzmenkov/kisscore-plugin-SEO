5626a392e765918aa633564c94b5daa1f3f277ba
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-07-18 16:35:28 +0700

    Allow to use custom domain passed as argument on generation file

--- Robots.php
+++ Robots.php
@@ -1,11 +1,14 @@
 <?php namespace Plugin\SEO;
 class Robots {
-  public static function generate($file, array $lines = []) {
+  public static function generate($file, array $lines = [], $domain = null) {
+    if (!$domain) {
+      $domain = config('common.domain');
+    }
     file_put_contents($file,
       'User-agent: *'
       . PHP_EOL . 'Crawl-delay: 1'
       . PHP_EOL . 'Host: ' . config('common.domain')
-      . PHP_EOL . 'Sitemap: ' . config('common.proto') . '://' . config('common.domain') . '/sitemap.xml'
+      . PHP_EOL . 'Sitemap: ' . config('common.proto') . '://' . $domain . '/sitemap.xml'
       . ($lines ? PHP_EOL . implode(PHP_EOL, $lines) : '')
     );
   }
--- Sitemap.php
+++ Sitemap.php
@@ -1,18 +1,21 @@
 <?php namespace Plugin\SEO;
 class Sitemap {
-  public static function generate($file, array $locs) {
+  public static function generate($file, array $locs, $domain = null) {
+    if (!$domain) {
+      $domain = config('common.domain');
+    }
     file_put_contents($file, '<?xml version="1.0" encoding="UTF-8"?>'
       . PHP_EOL . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0">'
       . PHP_EOL . '<url>'
-      . PHP_EOL . '  <loc>' . config('common.proto') . '://' . config('common.domain') . '</loc>'
+      . PHP_EOL . '  <loc>' . config('common.proto') . '://' . $domain . '</loc>'
       . PHP_EOL . '  <lastmod>' . gmdate('Y-m-d\TH:i:sP') . '</lastmod>'
       . PHP_EOL . '  <priority>1.0</priority>'
       . PHP_EOL . '</url>'
       . PHP_EOL . implode('',
-        array_map(function ($loc) {
+        array_map(function ($loc) use ($domain) {
           return
             PHP_EOL . '<url>'
-            . PHP_EOL . '  <loc>' . config('common.proto') . '://' . config('common.domain') . $loc['url'] . '</loc>'
+            . PHP_EOL . '  <loc>' . config('common.proto') . '://' . $domain . $loc['url'] . '</loc>'
             . PHP_EOL . '  <lastmod>' . $loc['date'] . '</lastmod>'
             . PHP_EOL . '  <priority>' . $loc['priority'] . '</priority>'
             . PHP_EOL . '</url>'
